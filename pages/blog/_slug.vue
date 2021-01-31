4b56d58476485d4462594744f162255d81d5db9c
ssghait007 <ssghait.007@gmail.com>
2021-01-31 16:14:56 +0530

    image size responsive

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,7 +4,7 @@
       class="container mx-auto flex flex-col px-5 py-24 justify-center items-center"
     >
       <img
-        class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+        class="lg:w-4/6 md:w-5/6 w-6/6 mb-10 object-cover object-center rounded"
         alt="hero"
         :src="post.image"
       />
