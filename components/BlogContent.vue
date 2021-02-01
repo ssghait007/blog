c43a4382d1fa5ca183b10dcc2d4215a909daf3fc
ssghait007 <ssghait.007@gmail.com>
2021-02-01 22:22:31 +0530

    href

--- components/BlogContent.vue
+++ components/BlogContent.vue
@@ -26,24 +26,24 @@
                 {{ post.description }}
               </p>
               <div class="flex items-center flex-wrap">
-                <NuxtLink :to="`/blog/${post.slug}`">
-                  <a
-                    class="text-indigo-500 hover:text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0"
-                    >Learn More
-                    <svg
-                      class="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M5 12h14"></path>
-                      <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                  </a>
-                </NuxtLink>
+                <a
+                  href="`/blog/${post.slug}`"
+                  class="text-indigo-500 hover:text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0"
+                  >Learn More
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+
                 <span
                   class="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 2 border-gray-200"
                 >
