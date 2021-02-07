6c55109f438b1cd4e46899d22eaade16dc5c2efa
ssghait007 <ssghait.007@gmail.com>
2021-02-07 18:56:25 +0530

    Added about pages

---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-col text-center w-full mb-12">
+        <h1
+          class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
+        >
+          Privacy policy
+        </h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+          Sample privacy policy, This will be updated soon.
+        </p>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+          A privacy policy (also referred to as a privacy notice) is a statement
+          or legal document that describes how a company, website, or app
+          collects, uses, maintains, and shares data collected from or about its
+          users. Most countries require any website that collects personal
+          information from its users to have a written privacy policy posted on
+          the website.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
