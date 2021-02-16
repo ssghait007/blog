9afcd8c0db5bfa5ff9d0b65975cd241d28467aeb
ssghait007 <ssghait.007@gmail.com>
2021-02-17 00:14:25 +0530

    form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -13,7 +13,12 @@
         </p>
       </div>
       <div class="lg:w-1/2 md:w-2/3 mx-auto">
-        <form name="contact" method="POST" data-netlify="true">
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
           <div class="flex flex-wrap -m-2">
             <div class="p-2 w-1/2">
               <div class="relative">
