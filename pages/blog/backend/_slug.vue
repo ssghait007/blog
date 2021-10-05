a7266614652df0b66cd0a794bc9a1d5078ec6d46
ssghait007 <ssghait.007@gmail.com>
2021-10-05 13:13:28 +0530

    updated slug with meta tags

--- pages/blog/backend/_slug.vue
+++ pages/blog/backend/_slug.vue
@@ -21,6 +21,22 @@
 <script>
 import { format } from 'date-fns'
 export default {
+  head() {
+    return {
+      meta: [
+        { hid: 'og-type', property: 'og:type', content: 'website' },
+        { hid: 'og-url', property: 'og:url', content: 'website' },
+        { hid: 'og-title', property: 'og:title', content: this.post.title },
+        {
+          hid: 'og-description',
+          property: 'og:description',
+          content: this.post.description,
+        },
+        { hid: 'og-image', property: 'og:image', content: this.post.image },
+      ],
+    }
+  },
+
   filters: {
     formatDate() {
       return format(new Date(), 'dd MMM yyyy')
