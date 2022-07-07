d240edf093bd725dd646ac21fb558389a6a33b69
Sachin Ghait <ssghait.007@gmail.com>
2022-07-07 19:09:28 +0530

    removed extra styles

--- components/BlogContent.vue
+++ components/BlogContent.vue
@@ -2,7 +2,11 @@
   <section class="text-gray-600 body-font">
     <div class="container px-5 py-12 mx-auto">
       <div v-if="filteredPosts.length" class="flex flex-wrap -m-4">
-        <div v-for="post in filteredPosts" :key="post.slug" class="p-4 md:w-1/3">
+        <div
+          v-for="post in filteredPosts"
+          :key="post.slug"
+          class="p-4 md:w-1/3"
+        >
           <div
             class="shadow-md h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden hover:shadow-md hover:rounded hover:border-purple-300 transition duration-300 transform hover:-translate-y-3"
           >
@@ -19,37 +23,50 @@
               >
                 {{ post.category }}
               </h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                {{ post.title }}
-              </h1>
-              <p class="leading-relaxed mb-3">
+              <nuxt-link
+                :to="`/blog/${post.slug}`"
+                class="text-indigo-500 hover:text-indigo-900 block items-center md:mb-2 lg:mb-0"
+              >
+                <h1
+                  class="title-font text-lg text-left font-medium text-gray-900 hover:text-gray-600"
+                >
+                  {{ post.title }}
+                </h1>
+              </nuxt-link>
+              <p class="leading-relaxed text-left">
                 {{ post.description }}
               </p>
-              <div class="flex items-center flex-wrap">
-                <nuxt-link
-                  :to="`/blog/${post.slug}`"
-                  class="text-indigo-500 hover:text-indigo-900 inline-flex items-center md:mb-2 lg:mb-0"
-                >
-                  Learn More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </nuxt-link>
 
-                <span
-                  class="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 2 border-gray-200"
-                >
-                  {{ post.createdAt | formatDate }}
-                </span>
+              <div
+                class="
+            flex flex-wrap
+            justify-starts
+            items-center
+            py-3
+            border-b-2
+            text-xs text-white
+            font-medium
+          "
+              >
+                <a v-for="t in post.tags" class="m-1 px-2 py-1 rounded bg-green-500" :key="t"> {{"#"+t.toUpperCase()}} </a>
+
+              </div>
+              <div class="flex items-center mt-2 flex-wrap text-left">
+                <img
+                  class="w-10 h-10 object-cover rounded-full"
+                  alt="User avatar"
+                  src="https://lh3.googleusercontent.com/a-/AFdZucogzmfN7i7Vbb3zeC77T3vz5TAOF4wI4fYihn2I=s80-p"
+                />
+                <div class="pl-2">
+                  <div class="font-medium">{{post.author}}</div>
+                  <div class="text-gray-600 text-xs">{{post.authorTitle}}</div>
+                </div>
+                <div class="ml-auto text-center">
+                  <div class="font-medium text-xs">{{post.readingTime}}</div>
+                  <div class="text-gray-600 text-xs">
+                    {{ post.createdAt | formatDate }}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -72,23 +89,24 @@ export default {
   filters: {
     formatDate(date) {
       return format(new Date(date), 'dd MMM yyyy')
-    },
+    }
   },
   props: {
     posts: {
       type: Array,
       default() {
         return []
-      },
-    },
+      }
+    }
   },
-  computed:{
-    filteredPosts(){
-      const show = typeof window !== 'undefined' ? localStorage.getItem('show') : null
-      if(show){
+  computed: {
+    filteredPosts() {
+      const show =
+        typeof window !== 'undefined' ? localStorage.getItem('show') : null
+      if (show) {
         return this.posts
       }
-      return this.posts.filter((a) =>  a.published)
+      return this.posts.filter(a => a.published)
     }
   }
 }
